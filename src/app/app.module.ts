cd300d80951e5f45a56133d0a3ebc186fead65d0
Upasnac <upasnachoudhary32@gmail.com>
2024-05-16 12:09:29 +1200

    addCesiumInAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CesiumMapDirective } from './cesium-map.directive';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CesiumMapDirective
   ],
   imports: [
     BrowserModule,
